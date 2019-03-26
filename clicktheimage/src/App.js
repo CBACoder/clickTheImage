7711de46f89fd3cd7f1ee28de53a3c3a46b020ae
CBACoder <46177147+CBACoder@users.noreply.github.com>
2019-03-26 00:19:26 -0500

    update

--- clicktheimage/src/App.js
+++ clicktheimage/src/App.js
@@ -8,38 +8,41 @@ import './App.css';
 
 class App extends Component {
   // set the state by using the array of our clickable images in imagesarray.json array
+  // the score is the click , the highestscore is tracker on every click of an image.
   state = {
     imagesarray,
     score: 0,
     highestscore: 0
   };
 
-  gameOver = () => {
+  restGame = () => {
     if (this.state.score > this.state.highestscore) {
-      this.setState({highestscore: this.state.score}, function() {
+      this.setState({ highestscore: this.state.score }, function () {
         console.log(this.state.highestscore);
       });
     }
+    // in the image array reset the count tracking to 0
     this.state.imagesarray.forEach(card => {
       card.count = 0;
     });
     console.log(`Game over and your score was: ${this.state.score}`);
-    this.setState({score: 0});
+    this.setState({ score: 0 });
     return true;
   }
 
   clickCount = id => {
-    this.state.imagesarray.find((k, i) => {
-      if (k.id === id) {
-        if(imagesarray[i].count === 0){
+    this.state.imagesarray.find((o, i) => {
+      if (o.id === id) {
+        if (imagesarray[i].count === 0) {
           imagesarray[i].count = imagesarray[i].count + 1;
-          this.setState({score : this.state.score + 1}, function(){
+          this.setState({ score: this.state.score + 1 }, function () {
             console.log(this.state.score);
           });
+          //randomize the array display
           this.state.imagesarray.sort(() => Math.random() - 0.5)
-          return true; 
+          return true;
         } else {
-          this.gameOver();
+          this.restGame();
         }
       }
     });
